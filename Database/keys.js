692fc518b4a59eb2a33c755fa0a78cde6bd03b9e
CarlosE-a11s <ninohilarion@yahoo.com>
2021-08-07 10:59:47 -0500

    actualizando nombre de DB en keys.js. Cambia Cluster0 por TourColombia

--- Database/keys.js
+++ Database/keys.js
@@ -1,5 +1,5 @@
 module.exports = { 
    mongoURI:
-      "mongodb+srv://CarlosNino_19:MiCluster01963@cluster0.kwuu0.mongodb.netCluster0?retryWrites=true&w=majority",
+      "mongodb+srv://CarlosNino_19:MiCluster01963@TourColombia.kwuu0.mongodb.netCluster0?retryWrites=true&w=majority",
    secretOrKey: "secret",
 };
